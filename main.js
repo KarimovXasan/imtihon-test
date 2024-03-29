7b6ef114a317788a12d3da884d21d43176110f48
XasanboyKarimov <xkarimov257@gmail.com>
2024-03-29 15:36:28 +0500

    doniyor

---
+++ main.js
@@ -0,0 +1,135 @@
+// 1-misol
+
+//  let a = 2;
+//  let b = 5;
+
+// if(a > b){
+//     console.log("a katta son");
+// } else{
+// //     console.log("b katta son");
+// //}
+
+// 2-misol
+
+// let a = 2;
+// let b = 5;
+
+// if(a > b){
+//     console.log("a katta son");
+//} else{
+//     console.log("b katta son");
+//}
+
+// 3-misol
+
+
+//let num = 6;
+//  if(num % 2 == 0 ){e.log("Siz juft son kitindingiz");
+//}else {
+//    console.log("Siz tog' sonlarni kiritdingiz");
+//}
+
+
+// 4-misol
+
+//B)  <script link="./script.js">
+
+
+// 5-misol
+//<!DOCTYPE html>
+//<html lang="en">
+//</html><head>
+//    <meta charset="UTF-8">
+//    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//    <title>Doniyor</title>
+//    <style>
+//        body {
+//            transition: background-color 0.5s ease;
+//        }
+//        .qora-background {
+//            background-color: black;
+//            color: white;
+//        }
+//   </style>
+//</meta></meta></head>
+//<body>
+//
+//<button class="btn">Button</button>
+//
+//<script>
+//    document.querySelector('.btn').addEventListener('click', function() {
+//        document.body.classList.toggle('qora-background');
+//    });
+//</script>
+//
+//</body>
+//</meta></html>
+
+
+// 6-misol
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Btn Exampl</title>
+//     <style>
+//         body {
+//            transition: background-color 0.5s ease;
+//    }
+//       .qora-background {
+//           background-color: black;
+//            color: white;
+//        }
+//    </style>
+//</meta></meta></head>
+//<body>
+//
+//<button class="btn">Button</button>
+//
+//<script>
+//    document.querySelector('.btn').addEventListener('click', function() {
+//        document.body.classList.toggle('qora-background');
+//    });
+//</script>
+
+//</body>
+//</meta></html>
+
+
+// 6-misol
+
+//.<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Toq Som</title>
+// </head>
+// <body>
+//
+// <script>
+//     for (let i = 1; i <= 20; i += 2) {
+//        document.write(i + "<br>");
+// }
+//</script>
+//
+//</body>
+//</meta></html>
+
+
+// 7-misol
+
+//C)    const btn = document.querySelector("btn");
+
+// 8-misol
+
+//B)   btn.style.backgroundColor = “red”;
+
+// 9-misol
+
+//C)   span, p , h1;
+
+// 10-misol
+
+//<link href="./style.css" />
